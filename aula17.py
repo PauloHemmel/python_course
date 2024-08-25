126c7fb4aeeef6daf718644a4e17ff3e4fe6b81a
PauloHemmel <paulogustavohemmel@gmail.com>
2024-08-25 18:02:35 -0400

    upgrade

---
+++ aula17.py
@@ -0,0 +1,15 @@
+# if / elif / else (condicionais): entendendo o fluxo do interpretador em condicionais
+
+# if / elif      / else
+# se / se não se / se não
+
+condicao = False
+
+if condicao:
+    print('Este é o código do if')
+else:
+    print('')
+
+if 10 == 10:
+    print('Outro if')
+print('Fora do if')
