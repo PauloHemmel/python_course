ebca91769033b4a5b2a335176c823e34fd73a0e3
PauloHemmel <paulogustavohemmel@gmail.com>
2024-08-27 23:18:36 -0400

    upgrade

--- aula17.py
+++ aula17.py
@@ -3,12 +3,28 @@
 # if / elif      / else
 # se / se não se / se não
 
-condicao = False
 
-if condicao:
-    print('Este é o código do if')
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+
+if condicao1:
+    print('código para condição 1')
+
+elif condicao2:
+    print('código para condição 2')
+
+elif condicao3:
+    print('código para condição 3')
+
+elif condicao4:
+    print('código para condição 4')
+
 else:
-    print('')
+    print('Nenhuma condição foi satisfeita.')
+
 
 if 10 == 10:
     print('Outro if')
