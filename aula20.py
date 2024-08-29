f446e1e8531c20ccdc66bf1d350dbf5e08ff5c5b
PauloHemmel <paulogustavohemmel@gmail.com>
2024-08-28 23:29:34 -0400

    upgrade

---
+++ aula20.py
@@ -0,0 +1,15 @@
+"""
+Exercicio de fixação
+
+"""
+
+
+
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'primeiro_valor=',primeiro_valor, 'é maior do que segundo_valor=',segundo_valor)
+else:
+    print(f'segundo_valor=',segundo_valor, 'é maior do que primeiro_valor=',primeiro_valor)
