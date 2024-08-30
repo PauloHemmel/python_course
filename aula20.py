e096e6858c592de4cdca5d648237523f886d4935
PauloHemmel <paulogustavohemmel@gmail.com>
2024-08-29 23:22:17 -0400

    upgrade

--- aula20.py
+++ aula20.py
@@ -9,7 +9,7 @@ Exercicio de fixação
 primeiro_valor = input('Digite um valor: ')
 segundo_valor = input('Digite outro valor: ')
 
-if primeiro_valor > segundo_valor:
-    print(f'primeiro_valor=',primeiro_valor, 'é maior do que segundo_valor=',segundo_valor)
+if primeiro_valor >= segundo_valor:
+    print(f'{primeiro_valor=} é maior ou igual' f' ao {segundo_valor=}')
 else:
-    print(f'segundo_valor=',segundo_valor, 'é maior do que primeiro_valor=',primeiro_valor)
+    print(f'{segundo_valor=} é maior ou igual' f' ao {primeiro_valor=}')
