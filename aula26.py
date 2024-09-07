5467bea71f6e200756b91ee78a74e6eab334bcfe
PauloHemmel <paulogustavohemmel@gmail.com>
2024-09-07 16:58:28 -0400

    Formatação de strings com f-strings

---
+++ aula26.py
@@ -0,0 +1,26 @@
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<numero de dígitos>f
+x e X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a
+"""
+
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.2345678765:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08x}')
+
+
+
