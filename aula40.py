1603b3e3c9cba2fb5ca7701046dc5955b1c136e5
PauloHemmel <paulogustavohemmel@gmail.com>
2024-10-02 22:26:18 -0400

    Exercício guiado - Calculadora - Parte 3

--- aula40.py
+++ aula40.py
@@ -5,6 +5,8 @@ while True:
     operador = input('Digite o operador (+-/*): ')
 
     numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
 
     try:
         num_1_float = float(numero_1)
@@ -27,6 +29,19 @@ while True:
     if len(operador) > 1:
         print('Digite apenas um operador.')
         continue
+
+    print('Realizando sua conta. Confira o resultado abaixo:')
+
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float} =',num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'{num_1_float} - {num_2_float} =',num_1_float - num_2_float)
+    elif operador == '*':
+         print(f'{num_1_float} * {num_2_float} =',num_1_float * num_2_float)
+    elif operador == '/':
+        print(f'{num_1_float} / {num_2_float} =',num_1_float / num_2_float)
+    else:
+        print('Não deveria aparecer isso aqui')
 
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
 
