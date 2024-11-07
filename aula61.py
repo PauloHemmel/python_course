b8ffa05bb4dd9f744062322aecbef7d6e37502fa
PauloHemmel <paulogustavohemmel@gmail.com>
2024-11-06 23:40:49 -0400

    Exercício - Gerar o primeiro dígito de um CPF com Python

--- aula61.py
+++ aula61.py
@@ -9,28 +9,39 @@ Ex.: 746.824.890-70
 
 
 """
+#Feito por Paulo
 
-entrada = input('Digite um CPF com 9 DI')
-indice = 0
-indice2 = 0
-indice3 = 1
-
-lista = [10, 9, 8]
-lista2 = []
-lista3 = []
-soma = 0
-for valores in entrada:
-    lista3.append(int(valores)*(lista[indice]))
+# entrada = input('Digite um CPF com 9 Dígitos: ')
+# indice = 0
+# lista = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+# lista3 = []
+# soma = 0
+# for valores in entrada:
+#     lista3.append(int(valores)*(lista[indice]))
      
-    indice += 1
-    
-while indice2 <= 2:
-    soma = lista3[indice2] + lista3[indice3] 
-    indice2 += 1
-    indice3 += 1
-
-print(lista3, soma)
-# while indice <= 3:
-#     resultado = (lista[indice])*(lista2[indice])
 #     indice += 1
-# print(resultado)
+
+# for item in lista3:
+#     soma += item
+
+# resultado = (soma * 10) % 11
+
+# if resultado > 9:
+#     final = 0
+#     print(f'{entrada}{final}0')
+# else:
+#     print(f'{entrada}{resultado}0')
+
+#Feito pelo Professor
+
+cpf = '74682489070'
+nove_digitos = cpf[:9]
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = ((resultado_digito_1 * 10) % 11)
+digito_1 = digito_1 if digito_1 <= 9 else 0
+print(digito_1)
